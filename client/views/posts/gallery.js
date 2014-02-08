00767a9adbdb0f99d20bb08cef2febe1670e3eba
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-02-08 19:57:42 +0100

    added basic gallery support

---
+++ client/views/posts/gallery.js
@@ -0,0 +1,82 @@
+var baseUrl = 'https://picasaweb.google.com/data/feed/api/user/:user/albumid/:albumId?alt=json&callback=?&v=2';
+
+var getAlbum = function () {
+	var salbumId = 'album' + this.username + this.albumId;
+
+	var data = Session.get(salbumId);
+	if (!data) {
+		var url = baseUrl.replace(/\:user/gi, this.username).replace(/\:albumid/gi, this.albumId);
+		$.getJSON(url).done(function(data) {
+			return Session.set(salbumId, data);
+		});
+	}
+	return data;
+};
+
+Template.galleryListItem.helpers(_.extend({
+	album: getAlbum
+}, i18n.templateHelperFor('title')));
+
+Template.galleryListItem.events({
+	'click .gallery-list-item': function () {
+		Router.go('gallery', this);
+	}
+});
+
+
+
+Template.galleryViewer.helpers({
+	selectedImage: function () {
+		var img = Session.get('gallerySelection');
+		var gh = Session.get('galleryH');
+		if (img) {
+			img = img.content.src.replace(/\/([^/]*)$/i, '/h' + gh + '/$1');
+			console.log(img);
+			return img;
+		} else {
+			return '/pix/loading.png';
+		}
+	}
+});
+
+Template.galleryViewer.rendered = function () {
+	$('.thumbnail', this.firstNode).css({height: Session.get('galleryH') + 10});
+};
+
+Template.thumbnails.helpers({
+	thumbnails: function () {
+		var data = getAlbum.call(this);
+		if (data) {
+			setTimeout(function () {
+				if (!Session.get('gallerySelection')) {
+					Session.set('gallerySelection', data.feed.entry[0]);
+				}
+			}, 1000);
+			return data.feed.entry;
+		}
+	},
+	image: function () {
+		var thumb = this.content.src;
+		var thumbSize = Session.get('thumbSize');
+		thumb = thumb.replace(/\/([^/]*)$/i, '/s' + thumbSize + '-c/$1');
+		return thumb;
+	}
+});
+
+Template.thumbnails.events({
+	'click .image': function () {
+		Session.set('gallerySelection', this);
+	}
+});
+
+var setSizes = function () {
+	Session.set('thumbSize', $(window).width() >= 1200 ? 110:90);
+
+	var vh = parseInt($(window).height() * 0.7);
+	Session.set('galleryH', vh);
+};
+
+Meteor.startup(function () {
+	setSizes();
+	$(window).on('resize', setSizes);
+});
--- client/views/posts/post.js
+++ client/views/posts/post.js
@@ -1,21 +1,3 @@
-var enrichText = function (s) {
-	s = s.replace(/\n\n/gi, '</p><p>');
-	s = s.replace(/\n/gi, '<br/>');
-	return '<p>' + s + '</p>';
-}
-
-Template.postSummary.helpers(_.extend({
-	'descriptionSummary': function () {
-		return this.description[i18n.language()].substr(0, 110);
-	}
-}, i18n.templateHelperFor('title', 'description')));
-Template.post.helpers(_.extend({}, 
-	i18n.templateHelperFor(
-		'title', 
-		{'description': enrichText 
-	})));
-
-
 var POST_FIELDS = {
 	title: {
 		default: fixie.fetchPhrase
@@ -31,9 +13,69 @@ var POST_FIELDS = {
 	},
 	weight: {
 		post: function (val) { return parseInt(val); },
-		hint: 'The heavier the element, the later it will appear'
+		hint: 'The heavier the element, the later it will appear',
+		default: 100
 	}
 };
+var GALLERY_FIELDS = {
+	title: {
+		default: fixie.fetchPhrase
+	},
+	username: {
+		type: 'text',
+		label: 'Google+ username',
+		hint: '(eg. write: johndoe if your gmail address is johndoe@gmail.com)',
+		default: 'albertmatyi'
+	},
+	albumId: {
+		type: 'text',
+		label: 'Google+ album ID',
+		hint: 'When you view an album, this the number that comes after "albums" in the url. Ex: https://plus.google.com/u/0/photos/110836571215849032642/albums/5940476182069855361',
+		default: '5940476182069855361'
+	},
+	weight: {
+		post: function (val) { return parseInt(val); },
+		hint: 'The heavier the element, the later it will appear',
+		default: 100
+	}
+};
+
+var editPost = function (category, post, postType, fields) {
+	var editor = new Editor({
+		'fields': i18n.editorFieldsDef(fields, ['title', 'description']),
+		'data': i18n.editorPrepData(post, ['title', 'description']),
+		'save': function (obj) {
+			obj = i18n.editorProcessData(obj);
+
+			if (post._id) {
+				PostCollection.update(post._id, {$set: _.pick(obj, _.keys(fields))});
+			} else {
+				PostCollection.insert(
+					_.extend({parent: category._id, type: postType},
+						_.pick(obj, _.keys(fields))
+						));
+			}
+		}
+	});
+	editor.show();
+};
+
+
+Template.postSummary.helpers(_.extend({
+	'descriptionSummary': function () {
+		return this.description[i18n.language()].substr(0, 110);
+	}
+}, i18n.templateHelperFor('title', 'description')));
+
+Template.post.helpers(_.extend({},
+	i18n.templateHelperFor('title', 'description')));
+
+Template.postEntry.helpers({
+	typeIs: function (type) {
+		return this.type === type;
+	}
+});
+
 Template.postControls.events({
 	'click .delete.btn': function () {
 		if(confirm('Are you sure?')) {
@@ -41,33 +83,21 @@ Template.postControls.events({
 		}
 	},
 	'click .edit.btn': function () {
-		var self = this;
-		var editor = new Editor({
-			'fields': i18n.editorFieldsDef(POST_FIELDS, ['title', 'description']),
-			'data': i18n.editorPrepData(this, ['title', 'description']),
-			'save': function (obj) {
-				obj = i18n.editorProcessData(obj);
-				PostCollection.update(self._id, {$set: _.pick(obj, _.keys(POST_FIELDS))});
-			}
-		});
-		editor.show();
+		if (this.type === 'gallery') {
+			editPost({}, this, 'gallery', GALLERY_FIELDS);
+		} else {
+			editPost({}, this, 'post', POST_FIELDS);
+		}
 	}
 });
 Template.postAdd.helpers(_.extend({}, i18n.templateHelperFor('title')));
+
 Template.postAdd.events({
 	'click .add.btn': function () {
-		var self = this;
-		var editor = new Editor({
-			'fields': i18n.editorFieldsDef(POST_FIELDS, ['title', 'description']),
-			'data': i18n.editorPrepData({}, ['title', 'description']),
-			'save': function (obj) {
-				obj = i18n.editorProcessData(obj);
-				PostCollection.insert(
-					_.extend({parent: self._id},
-						_.pick(obj, _.keys(POST_FIELDS))
-						));
-			}
-		});
-		editor.show();
+		if (this.type === 'gallery') {
+			editPost(this, {}, 'gallery', GALLERY_FIELDS);
+		} else {
+			editPost(this, {}, 'post', POST_FIELDS);
+		}
 	}
 });
