cf53189517367338004f9e58b42be7d4a070f575
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-02-09 17:00:19 +0100

    added google plus

--- client/views/flags/flags.js
+++ client/views/flags/flags.js
@@ -2,9 +2,10 @@ Template.flags.helpers({
 	flags: function () {
 		return [{ icon: 'play', type: 'live', text: 'live'},
 		{ icon: 'facebook-square', type: 'facebook'},
+		{ icon: 'google-plus', type: 'google-plus'},
+		{ icon: 'twitter-square', type: 'twitter'},
 		{ icon: 'tumblr-square', type: 'tumblr'},
-		{ icon: 'youtube-square', type: 'youtube'},
-		{ icon: 'twitter-square', type: 'twitter'}];
+		{ icon: 'youtube-square', type: 'youtube'}];
 	}
 });
 
