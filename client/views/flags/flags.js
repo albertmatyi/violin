cbae1fd23ba1da1d76578f290364a086a3b8f67a
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-02-09 16:36:48 +0100

    added social flags

---
+++ client/views/flags/flags.js
@@ -0,0 +1,9 @@
+Template.flags.helpers({
+	flags: function () {
+		return [{ icon: "play", text: 'live'},
+		{ icon: "facebook-square"},
+		{ icon: "tumblr-square"},
+		{ icon: "youtube-square"},
+		{ icon: "twitter-square"}];
+	}
+});
