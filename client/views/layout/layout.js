727f51495b8b23dab287a455dff82f0a6f71c48e
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-03-02 20:26:13 +0100

    fixed outer open

--- client/views/layout/layout.js
+++ client/views/layout/layout.js
@@ -1,6 +1,10 @@
 Template.layout.events({
 	'click a': function (evt) {
-		var dst = $(evt.target).prop('href');
+		var a = $(evt.target);
+		if (!a.is('a')) {
+			a = a.parents('a').first();
+		}
+		var dst = a.prop('href');
 		if (dst.indexOf('http') !== -1 && dst.indexOf(window.location.host) === -1) {
 			evt.preventDefault();
 			window.open(dst);
